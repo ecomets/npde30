bd8d22bd39a72e94386021b968e8f32622d4049c
Romain <60646503+MrRomainLeroux@users.noreply.github.com>
2021-02-01 13:21:04 +0100

    Add files via upload

--- notebooks/unitaryTests.R
+++ notebooks/unitaryTests.R
@@ -1,6 +1,6 @@
 ############################################
 # Setup
-testDir<-"/home/eco/work/npde/npde30/npde/tests/testthat" # folder
+testDir<-"D:/RECHERCHES/_NPDE/Npde-2021/npde30-main/npde/tests/testthat" # folder
 setwd(testDir)
 source("helper-source.R")
 
@@ -14,20 +14,35 @@ library(grid)
 
 ############################################
 # Classes and objects
-test_file("test-NpdeData-class.R")
+
+# ok
+test_file("test-NpdeData-class.R") 
+
+# ok
 test_file("test-NpdeRes-class.R")
+
+# 2 fails 
 test_file("test-NpdeObject-class.R")
+
+# ok
 test_file("test-NpdeSimData-class.R")
 
 ############################################
 # Binning
+# ok
 test_file("test-binning.R")
 
 # Plots
+# ok
 test_file("test-auxScatterPlots.R")
+
+# ok
 test_file("test-auxDistPlots.R")
 
+# ok
 test_file("test-scatterPlots.R")
+
+# ok
 test_file("test-distributionPlots.R")
 
 ############################################
